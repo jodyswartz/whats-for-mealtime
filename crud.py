4057e5d04ec1e7616d56c715e208c68960d9d031
Jody Swartz <jodyhswartz@hotmail.com>
2023-10-08 07:45:16 +0200

    minor change: Added parantheses

--- crud.py
+++ crud.py
@@ -3,7 +3,7 @@ import pymongo
 
 def insert(username, password, date, time, selected_name, selected_amount):
     # local
-    #url = 'mongodb://  :  @localhost:27017'.format(username, password)
+    # url = 'mongodb://  :  @localhost:27017'.format(username, password)
     # production
     url = f"mongodb+srv:// username : password @cluster0.m4cipub.mongodb.net/?tls=true&tlsAllowInvalidCertificates=true"
     client = pymongo.MongoClient(url)
@@ -16,7 +16,7 @@ def insert(username, password, date, time, selected_name, selected_amount):
 
     food_collection = db['food']
     # Production
-    food_collection.insert_one( 'date': date,
+    food_collection.insert_one({'date': date,
                                 'time': time,
                                 'name': selected_name,
-                                'amount': selected_amount )
+                                'amount': selected_amount})
